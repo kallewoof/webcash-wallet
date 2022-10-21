9e8686dda7b68f643e4e510d5d65745d47f35f67
Mark Friedenbach <mark@friedenbach.org>
2022-10-20 21:26:55 -0700

    Add support for building on Apple Silicon
    This bumps the version of libsha2 to fix a compilation error, and adds a separate config header for ARMv8 instead of x86_64.

--- WebcashWallet/libsha2-config.h
+++ WebcashWallet/libsha2-config.h
@@ -5,5 +5,9 @@
 #if TARGET_OS_IPHONE
 #include "libsha2-config-ios.h"
 #else
-#include "libsha2-config-mac.h"
+#if defined(__arm__) || defined(__aarch32__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM)
+#  include "libsha2-config-macos-arm.h"
+#elif defined(__x86_64__) || defined(__amd64__)
+#  include "libsha2-config-macos-x86.h"
+#endif /* ARM / X86 */
 #endif /* TARGET_OS_IPHONE */
