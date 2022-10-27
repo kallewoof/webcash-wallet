a86071defc8ba214af861c023b534f34da1b96c7
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2022-10-27 13:54:07 +0900

    test: disabled UI tests for now

--- WebcashWallet/Tests iOS/Tests_iOSLaunchTests.swift
+++ WebcashWallet/Tests iOS/Tests_iOSLaunchTests.swift
@@ -1,32 +1,32 @@
+////
+////  Tests_iOSLaunchTests.swift
+////  Tests iOS
+////
+////  Created by Karl-Johan Alm on 2022-10-19.
+////
 //
-//  Tests_iOSLaunchTests.swift
-//  Tests iOS
+//import XCTest
 //
-//  Created by Karl-Johan Alm on 2022-10-19.
+//class Tests_iOSLaunchTests: XCTestCase {
 //
-
-import XCTest
-
-class Tests_iOSLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
-}
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
+//
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//    }
+//
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
+//}
